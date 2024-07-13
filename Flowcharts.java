a5a579c042c0407ad023c53e6286eb55582de738
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2024-07-13 12:35:28 +0530

    first commit

---
+++ Flowcharts.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Flowcharts{
+    public static void main(String[] args) {
+        //input 
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        //output
+        System.out.println("Value input by user is");
+        System.out.print(a);
+        sc.close();
+        
+        
+    }
+
+}
+
+       
+        
+    
