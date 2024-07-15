f4ea6a05bcd129949a3de6d7a1f67b07b65862b4
RICHA CHAUDHARY <richa.chaudhary197@gmail.com>
2024-07-15 22:19:17 +0530

    Add Initial Conditions

---
+++ Loops.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+  public class Loops{ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+       // counter++ => counter = counter + 1
+     //  for (int counter = 0; counter < 11; counter ++ ) {
+       // System.out.println(counter);
+      // System.out.print(counter+" ");
+      //counter = i
+      //for ( int i = 0; i < 11; i++){
+        //System.out.println(i);
+      //}
+     // int n = sc.nextInt();
+      //int sum = 0;
+     // for (int i=0; i<=n; i++ ) {
+      //  sum = sum + i;
+      int n = sc.nextInt();
+      for(int i = 1; i < 11; i++) { 
+        System.out.println(i*n);
+
+      }
+
+    }
+}
+   // System.out.println(sum);
+
+
+      //while
+      //int i = 0;
+      //while(i < 11){
+       // System.out.println(i);
+       // i++;
+      //}
+      //do while
+      //int i = 12;
+      // while(i < 11) {
+     // System.out.println("Number");
+      //}
+      // do {
+      //System.out.println("Number");
+      //} while(i < 11);
+     // int i = 0;
+      //do {
+       // System.out.println(i);
+        // i++;
+        //} while(i < 11);
+       //}
+        /*int n = sc.nextInt();
+         /*for(int i =1;i<n; i++){
+            System.out.print(" * ");
+
+        }
+        System.out.println();
+        for(int i=1;i<n;i++){
+            System.out.print(i+" ");
+        } */
+    
+       /*for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++)
+           System.out.print( " * ");
+           System.out.println();
+       }/* */
+        /*int n = 4;
+        for (int i=1; i<=n; i++){
+        for (int k=1; k<=n-i; k++){
+           System.out.print("  ");
+        }
+        for (int j=1; j<=i; j++){
+           System.out.print( "* ");
+        }
+        System.out.println();
+    } */
+    /*int n = 4;
+    for (int i=n; i>=1;i--){
+    for (int j=1; j<=i;j++){
+        System.out.print("* ");
+    }  
+    System.out.println();
+    } */
+
+    /*int n = 4;
+    for (int i= n; i>=1; i--){
+    for (int k= 1; k<=n-i; k++){
+            System.out.print("    ");
+
+        }
+        for (int j= 1; j<=i; j++){
+        System.out.print( "* ");
+
+    }
+       System.out.println(); 
+    }/* */
+    
+    
+        
+ 
